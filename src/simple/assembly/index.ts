e0ff2fb3aaaf7d7862991bb81d4fc3c81c525725
Mücahit <mucahittanacioglu@gmail.com>
2022-04-18 19:51:19 +0300

    first-commit

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,77 @@
-import { storage, Context } from "near-sdk-as"
+import { context,logging} from "near-sdk-as"
+import {topics,Topic,Comment} from "./model"
+import { ONE_NEAR } from "../../utils"
+// variable holds topics and comments within
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
-}
 
-// read the given key from account (contract) storage
-export function read(key: string): string {
-  if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-  } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
+//constants
+const TOPIC_VIEW_LIMIT = 10
+const COMMENT_VIEW_LIMIT = 10
+const MAX_COMMENT_LENGTH_LIMIT = 100
+
+//function returns last topics
+export function get_topics(): Array<Topic> {
+  
+
+  const number_of_topic = min(TOPIC_VIEW_LIMIT, topics.length)
+  const starting_idx = topics.length - number_of_topic
+  const result = new Array<Topic>(number_of_topic)
+  
+  for(let i = 0; i < number_of_topic; i++) {
+    result[i] = topics[i + starting_idx]
   }
+
+  return result
 }
 
-// write the given value at the given key to account (contract) storage
-export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
+//function to create topics
+export function create_topic(name:string): void {
+
+  assert(!is_topic_exist(name), "Topic already created!")
+ 
+  const topic = new Topic(name)
+  topics.push(topic)
 }
 
-// private helper method used by read() and write() above
-function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+//function to add comment on topic
+export function add_comment_to_topic(topic_name:string,comment:string): void {
+  logging.log("Commment: "+comment + ` \nlength: ${comment.length}` )
+  assert(comment.length < MAX_COMMENT_LENGTH_LIMIT,`Comment too long max ${MAX_COMMENT_LENGTH_LIMIT} char!`)
+  assert(topics.length > 0 , "There is no topic created yet!")
+  assert(is_topic_exist(topic_name),"Topic does not exist!")
+  const topic_ = find_topic(topic_name)
+  assert(!(topic_.is_premium && context.attachedDeposit < ONE_NEAR), "Its a premium topic you need to attach at least 1 near!")
+  topic_.add_comment(new Comment(context.sender,comment))
+}
+
+//function that returns comments on given topic
+export function get_comments_on_topic(topic_name:string): Array<Comment> {
+  assert(is_topic_exist(topic_name),"Topic does not exist!")
+  const topic_ = find_topic(topic_name)
+
+  const number_of_comments = min(COMMENT_VIEW_LIMIT, topic_.comments.length)
+  const starting_idx = topic_.comments.length - number_of_comments
+  const result = new Array<Comment>(number_of_comments)
+  
+  for(let i = 0; i < number_of_comments; i++) {
+    result[i] = topic_.comments[i + starting_idx]
+  }
+  return result
+}
+
+// function to check whether topic exist
+function is_topic_exist(topic_name:string): boolean {
+  for(let i = 0; i < topics.length; i++){
+    if(topics[i].name == topic_name)
+      return true
+  }
+  return false
+}
+//function to find specific topic
+function find_topic(topic_name:string): Topic {
+  for(let i = 0; i < topics.length; i++){
+    if(topics[i].name == topic_name)
+      return topics[i]
+  }
+  return new Topic("This should not happen!!")
 }
---
+++ src/simple/assembly/model.ts
@@ -0,0 +1,29 @@
+import { u128, PersistentVector, context} from "near-sdk-as"
+import { ONE_NEAR } from "../../utils"
+//topic class
+@nearBindgen
+export class Topic{
+  name:string
+  comments:PersistentVector<Comment>
+  is_premium:boolean
+  constructor(t_name:string){
+    this.name = t_name
+    this.comments = new PersistentVector<Comment>("c")
+    this.is_premium = context.attachedDeposit >= ONE_NEAR
+  }
+  add_comment(comment:Comment): void {
+    this.comments.push(comment)
+  }
+}
+//comment class
+@nearBindgen
+export class Comment{
+  user:string
+  comment:string
+  constructor(account:string, comment_:string){
+    this.user = account
+    this.comment = comment_
+  }
+}
+
+export const topics = new PersistentVector<Topic>("t")
